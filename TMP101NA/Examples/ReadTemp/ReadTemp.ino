bc2825577c8a32df886bfd1bfe3d2162930096cb
Yadwinder Singh <ryker1990@gmail.com>
2017-01-13 10:00:11 +0530

    Update ReadTemp.ino

--- TMP101NA/Examples/ReadTemp/ReadTemp.ino
+++ TMP101NA/Examples/ReadTemp/ReadTemp.ino
@@ -3,7 +3,7 @@
         Distributed with a free-will license.
         Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
         TMP101NA
-        This code is designed to work with the TMP101NA_I2CADC I2C Mini Module available from ControlEverything.com.
+        This code is designed to work with the TMP101NA_I2CS I2C Mini Module available from ControlEverything.com.
         https://www.controleverything.com/content/Temperature?sku=TMP101NA_I2CS#tabs-0-product_tabset-2
 */
 /**************************************************************************/
