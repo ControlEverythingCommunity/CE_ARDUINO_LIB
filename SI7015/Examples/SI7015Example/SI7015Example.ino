240821c399aad1f6b9764d621c1d8702fc155588
Yadwinder Singh <ryker1990@gmail.com>
2017-01-13 09:56:56 +0530

    Update SI7015Example.ino

--- SI7015/Examples/SI7015Example/SI7015Example.ino
+++ SI7015/Examples/SI7015Example/SI7015Example.ino
@@ -3,7 +3,7 @@
         Distributed with a free-will license.
         Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
         SI7015
-        This code is designed to work with the SI7015_I2CADC I2C Mini Module available from ControlEverything.com.
+        This code is designed to work with the SI7015_I2CS I2C Mini Module available from ControlEverything.com.
         https://www.controleverything.com/content/Temperature?sku=SI7015_I2CS#tabs-0-product_tabset-2
 */
 /**************************************************************************/
