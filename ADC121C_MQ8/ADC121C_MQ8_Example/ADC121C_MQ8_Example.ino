5c92cece1055d68ef494611a47dcc2de54c9bd50
Yadwinder Singh <ryker1990@gmail.com>
2017-04-06 10:51:14 +0530

    Update ADC121C_MQ8_Example.ino

--- ADC121C_MQ8/ADC121C_MQ8_Example/ADC121C_MQ8_Example.ino
+++ ADC121C_MQ8/ADC121C_MQ8_Example/ADC121C_MQ8_Example.ino
@@ -5,6 +5,7 @@
         ADC121C_MQ8
         This code is designed to work with the ADC121C_I2CGAS_MQ8 I2C Mini Module available from ControlEverything.com.
         https://shop.controleverything.com/products/hydrogen-gas-sensor
+        NT
 */
 /**************************************************************************/
 
