703cf5b03e28a2d12817c5dcb811b23b1c81f394
Yadwinder Singh <ryker1990@gmail.com>
2017-04-06 11:38:04 +0530

    NT

--- TMD2671/Example/ReadProximity/ReadProximity.ino
+++ TMD2671/Example/ReadProximity/ReadProximity.ino
@@ -5,6 +5,7 @@
         TMD2671
         This code is designed to work with the TMD2671_I2CS I2C Mini Module available from ControlEverything.com.
         https://www.controleverything.com/content/Proximity?sku=TMD2671_I2CS#tabs-0-product_tabset-2
+        NT
 */
 /**************************************************************************/
 
