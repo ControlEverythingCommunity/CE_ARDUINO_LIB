b006e2e39649009619d93182868f309e964a8f8a
Yadwinder Singh <ryker1990@gmail.com>
2017-04-11 12:16:43 +0530

    NT

--- ADC121C_MQ7/ADC121C_MQ7.h
+++ ADC121C_MQ7/ADC121C_MQ7.h
@@ -4,7 +4,7 @@
         Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
         ADC121C_MQ7
         This code is designed to work with the ADC121C_I2CGAS_MQ7 I2C Mini Module available from ControlEverything.com.
-       https://shop.controleverything.com/products/carbon-monoxide-gas-sensor
+        https://shop.controleverything.com/products/carbon-monoxide-gas-sensor
 */
 /**************************************************************************/
 
@@ -112,7 +112,6 @@ typedef enum
     
 } ADC121C_MQ7AlertFlag_t;
 
-
 typedef enum
 {
     ALERT_PIN_DISABLE           = ADC121C_MQ7_REG_CONFIG_ALERT_PIN_DIS,
@@ -126,7 +125,6 @@ typedef enum
     POLARITY_HIGH               = ADC121C_MQ7_REG_CONFIG_POLARITY_HIGH
     
 } ADC121C_MQ7Polarity_t;
-
 
 
 class ADC121C_MQ7
@@ -191,7 +189,5 @@ class ADC121C_MQ7
         float Measure_RL()  { return 10.0; }
         // Clean Air Ratio
         float Measure_RoInCleanAir()  { return 27.0; }
-
-
 
 };
